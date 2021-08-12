c020db50794ac6e8d341af1ae94803f4a0137137
dfuehrer <dfuehrer@email.arizona.edu>
2021-08-11 20:43:14 -0700

    updated cpu percent server to display overall percent but graph each core individually (hardcoded to 16 for my laptop)

--- cpupercentServer.cpp
+++ cpupercentServer.cpp
@@ -9,7 +9,7 @@ void sigHandler(int signal);
 void setupSignals();
 
 
-using server_t = PercentGraphServer<unsigned long, 2, 1>;
+using server_t = PercentGraphServer<unsigned long, (16 + 1) * 2, 16, 1>;
 server_t * server_ptr = nullptr;
 
 
@@ -22,23 +22,59 @@ int main(const int argv, const char * argc[]){
     server.runServer([&server] (){
             std::ifstream statFile("/proc/stat");
             std::string cpu;
+            std::array<server_t::stored_t, (server_t::numGraphPercents + server_t::numPrintPercents) * 2> saves;
+            std::array<server_t::percent_t, server_t::numGraphPercents> percents;
 
-            unsigned long      user,   nice,   system,   idle,   iowait,   irq,   softirq,   steal;
-            statFile >> cpu >> user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal;
+            server_t::stored_t  user,   nice,   system,   idle,   iowait,   irq,   softirq,   steal,   tmp1,   tmp2;
+            //statFile >> cpu >>  user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal >> tmp1 >> tmp2;
+
+            //server_t::percent_t active = user + system + nice + softirq + steal;
+            //server_t::percent_t total  = active + idle + iowait;
+
+            //saves = server.getDatas();
+
+            //server_t::percent_t percent;
+            //if(total == saves[1]){
+            //    percent = 0;
+            //}else{
+            //    percent = ((active - saves[0]) * 100) / (total - saves[1]);
+            //}
+            //saves[0] = active;
+            //saves[1] = total;
+
+            saves = server.getDatas();
+
+            server_t::percent_t percent;
+            for(int i = 0; i < server_t::numGraphPercents + server_t::numPrintPercents; ++i){
+            //for(int i = 0; i < 1; ++i){
+                statFile >> cpu >>  user >> nice >> system >> idle >> iowait >> irq >> softirq >> steal >> tmp1 >> tmp2;
+
+                server_t::stored_t active = user + system + nice + softirq + steal;
+                server_t::stored_t total  = active + idle + iowait;
+
+                server_t::percent_t tmpPercent;
+                if(total == saves[i * 2 + 1]){
+                    tmpPercent = 0;
+                }else{
+                    tmpPercent = ((active - saves[i * 2]) * 100) / (total - saves[i * 2 + 1]);
+                }
+                //std::cerr << cpu << ": " << tmpPercent << ", " << active - saves[i * 2] << " " << total - saves[i * 2 + 1] << std::endl;
+                if(i){
+                    percents[i - 1] = tmpPercent;
+                }else{
+                    percent = tmpPercent;
+                }
+                saves[i * 2]     = active;
+                saves[i * 2 + 1] = total;
+
+            }
+
             statFile.close();
 
-            unsigned long active = user + system + nice + softirq + steal;
-            unsigned long total  = active + idle + iowait;
+            server.setPercents(percents, percent);
+            server.saveDatas(saves);
 
-            auto [pactive, ptotal] = server.getDatas();
-            //std::array<unsigned long, 2> stored = percentGraph.readDatas();
-            //unsigned long percent = (active - stored[0]) * 100 / (total - stored[1]);
-            server_t::percent_t percent = ((active - pactive) * 100) / (total - ptotal);
-
-            server.getPercents(percent);
-            server.saveDatas({active, total});
-
-            });
+    });
 
     server_ptr = nullptr;
 
